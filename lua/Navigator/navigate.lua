835e2cd530e310a56e2880896a961d1e62dfb58a
declancm <declanmullen3.14@gmail.com>
2022-04-08 11:50:06 +1000

    added optional circular nvim window movement

--- lua/Navigator/navigate.lua
+++ lua/Navigator/navigate.lua
@@ -84,22 +84,30 @@ function N.navigate(direction)
 
     -- Check if the old window and new window match after navigation
     local at_edge = cur_win == A.nvim_get_current_win()
+    local window_changed = cur_win ~= A.nvim_get_current_win()
 
     -- If only one tmux pane exists, perform
     -- 'circular movement' if on an edge
-    if N.config.circular_movement and at_edge and tmux.single_pane() then
-        if direction == 'h' then
-            cmd(vim.fn.winnr('$') .. ' wincmd w')
-        elseif direction == 'l' then
-            cmd('1 wincmd w')
+    if N.config.circular_movement and at_edge and tmux.single_pane(direction) then
+        local alt_directions = { h = 'l', j = 'k', k = 'j', l = 'h' }
+
+        local at_alt_edge = false
+
+        while not at_alt_edge do
+            local new_cur_win = A.nvim_get_current_win()
+            wincmd(alt_directions[direction])
+            at_alt_edge = new_cur_win == A.nvim_get_current_win()
         end
+
+        -- If the window has changed, don't change tmux pane
+        window_changed = cur_win ~= A.nvim_get_current_win()
     end
 
     -- If the old window and new window match
     -- then we can assume that we hit the edge
     -- there is tmux pane besided the edge
     -- So we can navigate to the tmux pane
-    if N.back_to_tmux(at_edge) then
+    if N.back_to_tmux(at_edge) and not window_changed then
         tmux.change_pane(direction)
 
         local save = N.config.auto_save
--- lua/Navigator/tmux.lua
+++ lua/Navigator/tmux.lua
@@ -48,10 +48,26 @@ function T.is_zoomed()
     return execute("display-message -p '#{window_zoomed_flag}'") == '1'
 end
 
----To check whether only one tmux pane exists
+---To check whether only one tmux pane exists in a direction
+---@param direction string
 ---@return boolean
-function T.single_pane()
-    return execute("display-message -p '#{window_panes}'") == '1'
+function T.single_pane(direction)
+    -- Check if only one tmux pane exists
+    if execute("display-message -p '#{window_panes}'") == '1' then
+        return true
+    end
+
+    -- Check if tmux pane is zoomed
+    if T.is_zoomed() and require('Navigator.navigate').config.disable_on_zoom then
+        return true
+    end
+
+    -- Compare dimensions of the tmux pane and tmux window in direction
+    if direction == 'h' or direction == 'l' then
+        return execute("display-message -p '#{pane_width}'") == execute("display-message -p '#{window_width}'")
+    elseif direction == 'j' or direction == 'k' then
+        return execute("display-message -p '#{pane_height}'") == execute("display-message -p '#{window_height}'")
+    end
 end
 
 return T
