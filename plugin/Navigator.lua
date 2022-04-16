b472da73598d79ec217eb4f8daeafb4d454dd96e
numToStr <vikasraj1911@gmail.com>
2022-04-16 15:23:56 +0530

    chore!: remove 0.6 support
    This will allow the following lua API in 0.7
    
    - `nvim_create_user_command` to create user commands
    - `nvim_create_{augroup,autocmd}` to create autocommands

--- plugin/Navigator.lua
+++ plugin/Navigator.lua
@@ -1,20 +1,9 @@
 local ucmd = vim.api.nvim_create_user_command
 
--- NOTE: remove this after 0.7 release
-if not ucmd then
-    vim.cmd([[
-        command NavigatorLeft lua require'Navigator'.left()
-        command NavigatorRight lua require'Navigator'.right()
-        command NavigatorUp lua require'Navigator'.up()
-        command NavigatorDown lua require'Navigator'.down()
-        command NavigatorPrevious lua require'Navigator'.previous()
-    ]])
-else
-    local N = require('Navigator')
+local N = require('Navigator')
 
-    ucmd('NavigatorLeft', N.left, {})
-    ucmd('NavigatorRight', N.right, {})
-    ucmd('NavigatorUp', N.up, {})
-    ucmd('NavigatorDown', N.down, {})
-    ucmd('NavigatorPrevious', N.previous, {})
-end
+ucmd('NavigatorLeft', N.left, {})
+ucmd('NavigatorRight', N.right, {})
+ucmd('NavigatorUp', N.up, {})
+ucmd('NavigatorDown', N.down, {})
+ucmd('NavigatorPrevious', N.previous, {})
