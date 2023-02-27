edcbdfb8dd213342573c8028f92254bb156bbf01
C.D. MacEachern <craig.daniel.maceachern@gmail.com>
2023-02-27 09:14:01 -0500

    previous window binding works now

--- lua/Navigator/mux/kitty/neighboring_window.py
+++ lua/Navigator/mux/kitty/neighboring_window.py
@@ -3,7 +3,9 @@ def main():
 
 
 def handle_result(args, result, target_window_id, boss):
-    boss.active_tab.neighboring_window(args[1])
-
+    if args[1] == 'p':
+        boss.active_tab.nth_window(-1)
+    else:
+        boss.active_tab.neighboring_window(args[1])
 
 handle_result.no_ui = True
--- lua/Navigator/mux/kitty/pass_keys.py
+++ lua/Navigator/mux/kitty/pass_keys.py
@@ -1,13 +1,14 @@
 import re
-
 from kittens.tui.handler import result_handler
 from kitty.key_encoding import KeyEvent, parse_shortcut
 
 
 def is_window_vim(window, vim_id):
     fp = window.child.foreground_processes
-    print(fp)
-    return any(re.search(vim_id, p['cmdline'][0] if len(p['cmdline']) else '', re.I) for p in fp)
+    return any(
+        re.search(vim_id, p["cmdline"][0] if len(p["cmdline"]) else "", re.I)
+        for p in fp
+    )
 
 
 def encode_key_mapping(window, key_mapping):
@@ -26,7 +27,7 @@ def encode_key_mapping(window, key_mapping):
     return window.encoded_key(event)
 
 
-def main():
+def main(args):
     pass
 
 
@@ -35,12 +36,16 @@ def handle_result(args, result, target_window_id, boss):
     window = boss.window_id_map.get(target_window_id)
     direction = args[2]
     key_mapping = args[3]
-    vim_id = args[4] if len(args) > 4 else "n?vim"
+    regex_to_match = "n?vim"
 
     if window is None:
         return
-    if is_window_vim(window, vim_id):
+
+    if is_window_vim(window, regex_to_match):
         encoded = encode_key_mapping(window, key_mapping)
         window.write_to_child(encoded)
     else:
-        boss.active_tab.neighboring_window(direction)
+        if direction == "prev":
+            boss.active_tab.nth_window(-1)
+        else:
+            boss.active_tab.neighboring_window(direction)
